8c10c0ae790e1cecd3f22bccd33d07faf4f43832
equipe09 <equipe09.capacitabr@gmail.com>
2025-05-14 19:01:13 -0300

    Initial Commit

---
+++ rpgdemesa2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rpgdemesa2
+//
+//  Created by iredefbmac_25 on 14/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rpgdemesa2/rpgdemesa2App.swift
@@ -0,0 +1,17 @@
+//
+//  rpgdemesa2App.swift
+//  rpgdemesa2
+//
+//  Created by iredefbmac_25 on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct rpgdemesa2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
